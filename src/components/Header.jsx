d5a8a1dbab41e7cc7ebf2d168dc0136c3347bdfb
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-03-26 19:31:13 -0600

    modified:   src/components/Header.jsx 	modified:   src/index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
 import logo from '../assets/Group.png'
-import search from '../assets/search.png'
-import shopping from '../assets/shopping.png'
-import user from '../assets/user.png'
 import banner from '../assets/Banner.png'
 import logoDes from '../assets/Rectangle.png'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUser, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
     return (
@@ -18,22 +16,23 @@ export default function Header() {
                     <a href="#" className=' bg-yellow-500 px-4 py-0.5 rounded-xl'>Promociones</a>
                     <a href="#" className=' hover:bg-yellow-500 px-4 py-0.5 rounded-xl' >Nosotros</a>
                 </div>
+
                 <div className='flex flex-row gap-6 py-2 px-3'>
-                    <div className=' cursor-pointer'>
-                        <img src={search} alt="" />
+                    <div className='cursor-pointer'>
+                        <FontAwesomeIcon icon={faSearch} className="text-black text-xl w-6 h-6" />
                     </div>
-                    <div className=' cursor-pointer'>
-                        <img src={user} alt="" />
+                    <div className='cursor-pointer'>
+                        <FontAwesomeIcon icon={faUser} className="text-black text-xl w-6 h-6" />
                     </div>
-                    <div className=' cursor-pointer'>
-                        <img src={shopping} alt="" />
-                        <span className="absolute top-4 right-3 mr-3 p-1 bg-red-600 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                    <div className='cursor-pointer relative'>
+                        <FontAwesomeIcon icon={faShoppingBag} className="text-black text-xl w-6 h-6" />
+                        <span className="absolute -top-1 -right-2 bg-red-600 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
                             {2}
                         </span>
                     </div>
                 </div>
             </header>
-            <section className="bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: `url(${banner})`, backgroundSize: '100%', height:'540px' }}>
+            <section className="bg-cover bg-center bg-no-repeat relative" style={{ backgroundImage: `url(${banner})`, backgroundSize: '100%', height: '540px' }}>
                 <div className='absolute top-0 right-20 bottom-0 flex w-1/3 p-5 items-center'>
                     <div className='w-full'>
                         <img src={logoDes} alt="" />
