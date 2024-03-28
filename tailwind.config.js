c243d66d91bc6af57d548bc473fa41fd12b3f5cf
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-03-28 12:01:06 -0600

    modified:   index.html 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Kumbh Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
